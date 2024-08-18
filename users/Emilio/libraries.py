13a4a83170a1103c7e32a9a5f46a819db8c5b7d5
EmiRodGut <emiliorguez@gmail.com>
2024-08-18 18:15:57 +0100

    Carpeta Emilio

---
+++ users/Emilio/libraries.py
@@ -0,0 +1,20 @@
+
+import pandas as pd
+import numpy as np
+
+# Visualización de datos
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+
+# Expresiones regulares
+import re
+from fuzzywuzzy import process
+
+import warnings
+warnings.filterwarnings('ignore')
+
+def import_libraries():
+    print("Librerías importadas: pandas as pd, numpy as np, matplotlib.pyplot as plt, plotly.express as px, seaborn as sns, re, process, warnings")
+if __name__ == "__main__":
+    import_libraries()
